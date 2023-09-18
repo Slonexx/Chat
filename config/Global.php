de0410833a55bc0bbc8055063158c5f491f208ec
Slonexxx <21moriarty21@mail.ru>
2023-09-18 12:57:13 +0600

    create bd view controller, update popup

--- config/Global.php
+++ config/Global.php
@@ -7,6 +7,6 @@ return [
     'appUid' => env('APP_UID'),
     'secretKey' => env('SECRET_KEY'),
 
-    'moyskladVendorApiEndpointUrl' =>  'https://online.moysklad.ru/api/vendor/1.0',
+    'moyskladVendorApiEndpointUrl' =>  'https://apps-api.moysklad.ru/api/vendor/1.0.',
     'moyskladJsonApiEndpointUrl' =>  'https://api.moysklad.ru/api/remap/1.2',
 ];
