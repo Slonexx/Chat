715aac8992f034b539500b97eba083cadba55c08
Slonexxx <21moriarty21@mail.ru>
2023-11-08 11:48:31 +0600

    new

--- resources/views/popup/ViewPopap.blade.php
+++ resources/views/popup/ViewPopap.blade.php
@@ -9,7 +9,7 @@
                 sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation"
                 allow="camera https://dialogs.pro/; microphone https://dialogs.pro/; clipboard-read https://dialogs.pro/; clipboard-write https://dialogs.pro/"
                 width="100%"
-                height="99%"
+                height="105%"
                 style="border: 0;" >
             </iframe>
         </div>
