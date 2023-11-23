95241e940c9fea9baf5a694c1a0c6feba6b71e78
Slonexxx <21moriarty21@mail.ru>
2023-11-23 21:13:52 +0600

    new

--- resources/views/popup/ViewPopap.blade.php
+++ resources/views/popup/ViewPopap.blade.php
@@ -1,7 +1,7 @@
 @extends('popup.index')
 @section('content')
 
-    <div class="main-container" style="height: 100%">
+    <div class="main-container">
         <div class="content-container">
             <div>
                 <iframe
